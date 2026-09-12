974ed7777581ad1fc5ec9376093836304db29749
Marton Sziksz <six.marton070122@gmail.com>
2026-09-12 18:02:38 +0200

    Added steps to Recipe class

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -3,10 +3,12 @@ import 'ingredient.dart';
 class Recipe {
   final String name;
   final List<Ingredient> ingredients;
+  final List<String> steps;
 
   Recipe({
     required this.name,
-    required this.ingredients
+    required this.ingredients,
+    required this.steps
   });
 
 }
