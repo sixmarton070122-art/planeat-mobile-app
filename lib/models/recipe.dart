353a4f3751ad0d9b9bb3ca11d9507eaa00445e54
Marton Sziksz <six.marton070122@gmail.com>
2026-09-12 18:07:56 +0200

    Add servings, totalCost and prepTime to Recipe

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -2,11 +2,17 @@ import 'ingredient.dart';
 
 class Recipe {
   final String name;
+  final int servings;
+  final double prepTime;
+  final double totalCost;
   final List<Ingredient> ingredients;
   final List<String> steps;
 
   Recipe({
     required this.name,
+    required this.servings,
+    required this.prepTime,
+    required this.totalCost,
     required this.ingredients,
     required this.steps
   });
