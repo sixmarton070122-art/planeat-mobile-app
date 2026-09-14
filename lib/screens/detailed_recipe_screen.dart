472dfb3477e5c60c53b6feb08ee0764c5bf2336b
Marton Sziksz <six.marton070122@gmail.com>
2026-09-14 17:10:30 +0200

    Removing unused imports

--- lib/screens/detailed_recipe_screen.dart
+++ lib/screens/detailed_recipe_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:planeat_mobile_app/models/recipe.dart';
