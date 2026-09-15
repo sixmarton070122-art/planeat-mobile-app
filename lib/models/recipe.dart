96235bbf09991872b05bdadcd7d71828280a92ad
Marton Sziksz <six.marton070122@gmail.com>
2026-09-15 11:17:33 +0200

    Recipe factory added

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:planeat_mobile_app/models/ingredient.dart';
 import 'package:planeat_mobile_app/models/cooking_step.dart';
 
@@ -17,7 +18,24 @@ class Recipe {
     required this.totalCost,
     required this.ingredients,
     required this.steps,
-    required this.imageURL
+    required this.imageURL,
   });
 
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      name: json['name'] as String,
+      servings: json['servings'] as int,
+      timeToCook: json['timeToCook'] as double,
+      totalCost: json['totalCost'] as double,
+      ingredients: List<Ingredient>.generate(
+        json['ingredients'].length,
+        (index) => Ingredient.fromJson(json['ingredients'][index]),
+      ),
+      steps: List<CookingStep>.generate(
+        json['steps'].length,
+        (index) => CookingStep.fromJson(json['steps'][index]),
+      ),
+      imageURL: json['imageURL'],
+    );
+  }
 }
